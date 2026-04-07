6a9d4e6f908ecc131dfc24356506a5eef7dc93e9
Akko <131815569+Bumh3rr@users.noreply.github.com>
2026-04-07 07:05:33 -0600

    feat ( Navigation Botom bar y pages restantes )

---
+++ src/pages/Tienda/TiendaPage.tsx
@@ -0,0 +1,10 @@
+import styles from './TiendaPage.module.css'
+
+export default function TiendaPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.background} />
+      <h1 className={styles.title}>Tienda</h1>
+    </div>
+  )
+}
