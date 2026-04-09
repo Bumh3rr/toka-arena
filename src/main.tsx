6e2ee9b115feb3ea91a789eed5846702d6ae2c5e
Akko <131815569+Bumh3rr@users.noreply.github.com>
2026-04-09 02:47:50 -0600

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 //import App from './App'
-import TestPage from './pages/test/PagosTest'
-//import GetAuthCode from './pages/test/GetAuthCode'
+//import TestPage from './pages/test/PagosTest'
+import GetAuthCode from './pages/test/GetAuthCode'
 
 createRoot(document.getElementById('root')!).render(
-    //<GetAuthCode />
+    <GetAuthCode />
     //<App />
-    <TestPage />
+    //<TestPage />
 )
