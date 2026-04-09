ada80cc47f392e95fb7d8ca42d1cd01efc73af3d
Akko <131815569+Bumh3rr@users.noreply.github.com>
2026-04-09 13:14:58 -0600

    test

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -13,21 +13,32 @@ export function useAuth() {
     setLoading(true)
     setError(null)
     try {
+      authCode  = ''
       // 1. Obtener authCode de la URL (inyectado por Toka Super App)
       //const authCode = getAuthCodeFromURL()
-      authCode = 'DEBUG' // TODO: Eliminar esta línea y usar el authCode real inyectado por la Super App. Se deja hardcodeado para facilitar pruebas sin necesidad de la Super App. --- IGNORE ---
+     // authCode = 'DEBUG' // TODO: Eliminar esta línea y usar el authCode real inyectado por la Super App. Se deja hardcodeado para facilitar pruebas sin necesidad de la Super App. --- IGNORE ---
 
       // 2. Intercambiar authCode por JWT en el backend
-      const response = await authService.loginWithAuthCode(authCode)
+      //const response = await authService.loginWithAuthCode(authCode)
 
-      if (!response.success) throw new Error('Login fallido')
+      //if (!response.success) throw new Error('Login fallido')
 
       // 3. Guardar sesión completa
-      authService.saveSession(response)
+      authService.saveSession({
+        success: true,
+        accessToken: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0IiwiaWF0IjoxNzc1NzYyMDA4LCJleHAiOjE3NzU4NDg0MDh9.3kK3f3GEIL7Di2xCJSvEpt2tNMR8RXzcoddOkr-ArI4wt7jNR67OjRcAYYx2uGmCiDZHjnkbLuzxeET3FCieCQ',
+        tokenType: 'Bearer',
+        user: {
+          id: 1,
+          username: 'demo_user',
+          hasFirstToka: false,
+          tf: 0
+        }
+      })
 
       return {
         success: true,
-        hasFirstToka: response.user.hasFirstToka
+        hasFirstToka: false
       }
     } catch (err: any) {
       const msg = err.response?.data?.message || err.message || 'Error al iniciar sesión con Toka'
