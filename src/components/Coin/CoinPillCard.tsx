7779d1ec1841a2ae71bb1b411a43977023c1685b
Akko <131815569+Bumh3rr@users.noreply.github.com>
2026-06-03 13:24:09 -0600

    feat: add bump prop to CoinPillCard

--- src/components/Coin/CoinPillCard.tsx
+++ src/components/Coin/CoinPillCard.tsx
@@ -1,16 +1,16 @@
-
 import styles from './CoinPillCard.module.css'
 
 interface CoinPillCardProps {
-    tf: number
+  tf: number
+  bump?: boolean
 }
 
-export default function CoinPillCard({ tf }: CoinPillCardProps) {
-    return (
-        <div className={styles.coinPill}>
-            <img src="/assets/ui/moneda_tf.svg" alt="TF" className={styles.bump} />
-            <span>{tf}</span>
-            <span className={styles.tf}>TF</span>
-        </div>
-    )
+export default function CoinPillCard({ tf, bump = false }: CoinPillCardProps) {
+  return (
+    <div className={styles.coinPill}>
+      <img src="/assets/ui/moneda_tf.svg" alt="TF" className={bump ? styles.bump : ''} />
+      <span>{tf}</span>
+      <span className={styles.tf}>TF</span>
+    </div>
+  )
 }
