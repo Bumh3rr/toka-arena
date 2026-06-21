3e1a4fb73dba2537685a526400631ae102d09ed8
Akko <131815569+Bumh3rr@users.noreply.github.com>
2026-06-20 23:25:45 -0600

    test

--- src/shared/session/api/auth.api.ts
+++ src/shared/session/api/auth.api.ts
@@ -4,7 +4,7 @@ import type { AuthApi } from "../model/types";
 
 export const realAuthApi: AuthApi = {
   async login(authCode: string) {
-    const body: LoginSuperAppRequestDTO = { authCode };
+    const body: LoginSuperAppRequestDTO = { authCode: authCode };
     const { data } = await api.post<AuthResponseDTO>('/auth/login', body);
     return data;
   },
